174a8730bb0de8d385398bc9cc76648fdac4dd72
xiaokang00010 <zhouyongkang4321@gmail.com>
2020-09-12 21:21:08 +0800

    Test

---
+++ XBlog.js
@@ -0,0 +1,10 @@
+function init_blog(string config_path){
+  var result = new Object;
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.open("GET",config_path,true);
+  xmlhttp.send(null);
+  var toWrite = document.getElementById("embedded");
+  if(toWrite == null){
+    document.body = "<font color=\"red\"><b>Framework Error: </b></font>Can not fetch config file!";
+  }
+}
