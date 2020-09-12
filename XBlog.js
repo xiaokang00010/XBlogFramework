7181abd33fa8a8e54b172962c66ef12b5e2a6cc1
xiaokang00010 <zhouyongkang4321@gmail.com>
2020-09-13 07:49:14 +0800

    Commit

--- XBlog.js
+++ XBlog.js
@@ -1,4 +1,15 @@
 var toWrite = document.getElementById("embedded");
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
 function init_blog (config_path){
   var result = new Object;
   var xmlhttp=new XMLHttpRequest();
@@ -11,4 +22,12 @@ function init_blog (config_path){
   }
   var json = JSON.parse(xmlhttp.responseText);
   console.log(json);
+  result.config = json;
+  console.log("Framework Info: Blog init success!\n");
+}
+
+function parseURIArg(blog_obj){
+  string action = getQueryVariable("action");
+  string issue_id = getQueryVariable("issue_id");
+  if(action == "")  window.location = blog_obj.config["root"] + "index.html?action=home";
 }
